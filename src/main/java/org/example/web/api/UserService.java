6c975afedf3b8ce0a7082f1104c5240d1cf675a0
milkcandy <milkcandyxxxx@gmail.com>
2025-11-10 19:06:33 +0800

    优化

--- src/main/java/org/example/web/api/UserService.java
+++ src/main/java/org/example/web/api/UserService.java
@@ -4,7 +4,7 @@ import org.example.web.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+
 
 @Service
 public class UserService {
